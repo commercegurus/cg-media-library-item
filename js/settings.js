136194fec655c4ea4f3baefb521e1857e5a5e81d
Colm Troy <colmtroy@gmail.com>
2025-03-23 19:41:48 +0000

    feat: add JavaScript for settings page functionality
    - Introduced a new settings.js file to handle the initialization of color pickers on the settings page.
    - Ensured compatibility with jQuery and WordPress color picker functionality.

---
+++ js/settings.js
@@ -0,0 +1,11 @@
+/**
+ * Settings page JavaScript
+ */
+(function ($) {
+  "use strict";
+
+  // Initialize color pickers
+  $(function () {
+    $(".cg-color-picker").wpColorPicker();
+  });
+})(jQuery);
